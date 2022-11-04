8207c22b7f07386c80b9a1083ce26d0eec105cad
kipyegonbiegon <kipyegonbiegon@gmail.com>
2022-11-04 10:05:18 +0300

    tourism folder with project source code

---
+++ admin/about.php
@@ -0,0 +1,73 @@
+<?php
+require_once('check_login.php');
+?>
+<?php include"header.php"?>
+<!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor or Laravel work contact me at mayuri.infospace@gmail.com  -->
+<?php include"sidebar.php"?>
+        
+        <div class="page-wrapper">
+            
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">About Author</h3> </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">About Author</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+            
+            
+            <div class="container-fluid">
+                
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card card-outline-primary">
+                            
+                            <div class="card-body">
+                               
+                                    <div class="form-body">
+                                        <h3 class="card-title m-t-15"><strong>Basic Info</strong>   </h3>
+                                        <hr>
+                      <h4>                  Name:    Komolkori Biegon.
+                        <br>
+Nationality:    Kenyan
+                        </h4>      <br>
+                        <h3 class="card-title m-t-15"><strong>Eduction & Knowledge</strong>   </h3>
+                        <hr>
+                      <h4>                  Education:  Undergraduate in Information Technology<br>
+Programming Language:   PHP,C,Java and Pythoooon!
+                        </h4>      
+
+                         <br>
+                        <h3 class="card-title m-t-15"><strong>Work Experience</strong>   </h3>
+                        <hr>
+                      <h4>                  Doing Remote tasks when free.
+                        </h4>  
+<br><br>
+                        <h3 class="card-title m-t-15"><strong>Note :</strong>   </h3>
+                        <hr>
+                      <h4><strong><mark>As Undergraduates we are still looking for more projects 
+                          to</br> create/redesign in order to sell to customers who needs <br><br>
+
+</mark>
+</strong>                        </h4>      
+                           
+                                        
+
+                                       
+                                    </div>
+                                   
+                               
+                            </div>
+                        </div>
+                
+ </div>
+</div>
+</div>
+                   
+          <!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor or Laravel work contact me at mayuri.infospace@gmail.com  -->
+<?php include"footer.php"?>
